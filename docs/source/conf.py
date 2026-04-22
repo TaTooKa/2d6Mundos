fe348da4d060cb3646f39f8ca97e65e6823b2297
TaTooKa <leandro.rolando@gmail.com>
2026-04-22 02:11:10 -0300

    setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,7 @@ hoverxref_auto_ref = True
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = "_static/images/Erzahlspiel-logo.png"
+html_logo = "_static/images/2d6mundos-logo.png"
 html_theme_options = {
     'logo_only': True,
     'display_version': True,
