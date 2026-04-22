776f795c0abd0fdfb63c15b8d036c827c1bffc73
TaTooKa <leandro.rolando@gmail.com>
2026-04-22 01:58:05 -0300

    setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'hoverxref.extension',
 ]
 
 intersphinx_mapping = {
@@ -29,7 +30,31 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
+# html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# -- Options for hoverxref
+hoverxref_roles = [
+    'numref',
+    'confval',
+    'setting',
+    'footnote',
+]
+hoverxref_auto_ref = True
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+html_logo = "_static/images/Erzahlspiel-logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': True,
+    "use_sidenotes": True,
+}
+
+def setup(app):
+    app.add_css_file('css/custom.css')
